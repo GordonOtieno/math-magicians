ff18663d51ce50902e7d675be69dfdcad9514cf6
GordonOtieno <otienogordon95@gmail.com>
2022-09-06 16:49:37 +0300

    update template files

---
+++ src/components/Calculator.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../App.css';
+
+class Calculator extends React.PureComponent {
+  render() {
+    return <di>Hi calculator</di>;
+  }
+}
+
+export default Calculator;
