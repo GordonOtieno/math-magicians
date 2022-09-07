3472405f1356ed507a724945f62349c3770f0506
GordonOtieno <otienogordon95@gmail.com>
2022-09-07 20:24:39 +0300

    add action handlers

---
+++ src/components/ButtonContainer.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from './Button';
+
+const ButtonPanel = ({ handleClick }) => {
+  const group1 = ['AC', '+/-', '%', '÷'];
+  const group2 = ['7', '8', '9', 'x'];
+  const group3 = ['4', '5', '6', '-'];
+  const group4 = ['1', '2', '3', '+'];
+  const group5 = ['0', '.', '='];
+
+  const terFunc = (button, group) => (button === group[group.length - 1] ? (
+  // if the button is the last element in group
+    <Button
+      name={button}
+      key={button}
+      width={false}
+      color="#ff8000"
+      handleClick={handleClick}
+    />
+  ) : (
+    <Button
+      name={button}
+      key={button}
+      width={false}
+      handleClick={handleClick}
+    /> // picks the default color
+  ));
+
+  const buttonGroup = (group) => group.map((button) => (button === '0' ? ( // If button is 0 set the width to 50%
+    <Button name={button} key={button} width handleClick={handleClick} />
+  ) : (
+    terFunc(button, group)
+  )));
+
+  return (
+    <div className="calculatorPanel bPanel-width">
+      <div className="group1">{buttonGroup(group1)}</div>
+      <div className="group2">{buttonGroup(group2)}</div>
+      <div className="group3">{buttonGroup(group3)}</div>
+      <div className="group4">{buttonGroup(group4)}</div>
+      <div className="group5">{buttonGroup(group5)}</div>
+    </div>
+  );
+};
+
+ButtonPanel.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+};
+export default ButtonPanel;
